299d2fad11eeeb9b8e4005a54dbe6092a4875d12
keithchasen <keithchasen89@gmail.com>
2024-02-09 15:38:09 +0100

    set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { Chat } from './pages/Chat'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
 function App() {
 
   return (
     <>
-      SOCKETS
+      <Routes>
+        <Route path='/' element={<Chat/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
     </>
   )
 }
 
 export default App
-``
