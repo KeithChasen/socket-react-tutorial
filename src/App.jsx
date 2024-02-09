9b9209691450766aeabf32114d72fb6d6562ed3c
keithchasen <keithchasen89@gmail.com>
2024-02-09 16:41:25 +0100

    using auth context user to show particular content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { useContext } from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import { NavBar } from './components/NavBar'
+import { AuthContext } from './context/AuthContext'
 import { Chat } from './pages/Chat'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
 function App() {
 
+  const { user } = useContext(AuthContext)
+
   return (
     <>
       <NavBar/>
       <Routes>
-        <Route path='/' element={<Chat/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
+        <Route path='/' element={user ? <Chat/> : <Navigate to="/login" />} />
+        <Route path='/login' element={user ? <Navigate to="/login" /> : <Login/>} />
+        <Route path='/register' element={user ? <Navigate to="/login" /> : <Register/>} />
         <Route path='*' element={<Navigate to="/" />} />
       </Routes>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,25 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 
 export const NavBar = () => {
+    const { user } = useContext(AuthContext)
+
     return (
         <nav>
             <ul>
-                <Link to='/'>Chat</Link>
-                <Link to='/register'>Register</Link>
-                <Link to='/login'>Login</Link>
+                { user && <Link to='/'>Chat</Link> }
+                {user ? 
+                <div>
+                    <span>{user.email}</span>
+                    <button>Logout</button> 
+                </div>
+                : (
+                    <div>
+                        <Link to='/register'>Register</Link>
+                        <Link to='/login'>Login</Link>
+                    </div>
+                )}
             </ul>
         </nav>
     )
