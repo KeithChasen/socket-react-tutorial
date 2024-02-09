1db1e293eaad0b7b9583aa66f80bb1afcfa6b487
keithchasen <keithchasen89@gmail.com>
2024-02-09 15:49:43 +0100

    login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,17 @@
 export const Login = () => {
-    return <>Login</>
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <label>Email</label>
+                <input type="email" />
+
+                <label>Password</label>
+                <input type="password" />
+
+                <button>Login</button>
+            </form>
+        </div>
+    )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,17 @@
 export const Register = () => {
-    return <>Register</>
+    return (
+        <div>
+            <h1>Register</h1>
+            <form>
+                <label>Email</label>
+                <input type="email" />
+
+                <label>Password</label>
+                <input type="password" />
+
+                <button>Register</button>
+            </form>
+        </div>
+    )
 }
 
